3d8e294df1a9adc6425db974a199f915c9cf2cff
mohi14 <mohiuddinkhan1409@gmail.com>
2023-07-27 16:18:15 +0600

    make contact modal and controller done

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -2,11 +2,17 @@ const express = require("express");
 const {
   registerUser,
   emailVirification,
+  loginUser,
+  getAllUsers,
+  deleteUser,
 } = require("../controller/users.controller");
 
 const router = express.Router();
 
 router.post("/signup", registerUser);
+router.post("/login", loginUser);
+router.get("/", getAllUsers);
+router.delete("/delete/:id", deleteUser);
 router.get("/verify/:email", emailVirification);
 
 module.exports = router;
